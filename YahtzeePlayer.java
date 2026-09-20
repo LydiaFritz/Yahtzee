d87f9ee9d266d62518b35750b15a861fd8088167
Lydia <Lydia.Fritz@gcu.edu>
2026-09-20 12:31:21 -0400

    added YahtzeePlayer

--- YahtzeePlayer.java
+++ YahtzeePlayer.java
@@ -1,3 +1,25 @@
-public class YahtzeePlayer {
+/**
+ * 
+ * YahtzeePlayer
+ * Lydia Fritz
+ */
+
+public class YahtzeePlayer extends Player{
+    
+    private ScoreCard scoreCard;
+
+    public YahtzeePlayer(String n){
+        //initialize the Player name
+        super(n);
+        //initialize the scoreCard
+        scoreCard = new ScoreCard();
+    }
+
+    public void showScoreCard(){
+        System.out.printf("Upper Section for %s%n", this.getName());
+        this.scoreCard.showUpperSection();
+        System.out.printf("%nLower Section for %s%n", this.getName());
+        this.scoreCard.showLowerSection();
+    }
     
 }
